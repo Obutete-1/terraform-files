1ff52c0a66e581d810fe9fd154fbf371bfa30b20
Obutete-1 <gabrielobute1@gmail.com>
2025-06-06 23:32:31 +0000

    tf module

---
+++ moodles/security/variables.tf
@@ -0,0 +1,29 @@
+variable "project_name" {
+  description = "Name of the project"
+  type        = string
+}
+
+variable "environment" {
+  description = "Environment name"
+  type        = string
+}
+
+variable "vpc_id" {
+  description = "VPC ID"
+  type        = string
+}
+
+variable "default-route" {
+  description = "default"
+  type        = string
+}
+
+variable "portnumber" {
+  description = "Security Group Port Number"
+  type        = set(string)
+}
+
+variable "my_ip" {
+  description = "MY Public IPv4 address"
+  type        = string
+}
